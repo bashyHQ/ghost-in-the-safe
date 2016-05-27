d26256d60909d7c5971de299070520764620f9fc
Benjamin Kampmann <ben@create-build-execute.com>
2016-05-27 22:39:14 +0200

    DNS setup feature added

--- app/configuration.jsx
+++ app/configuration.jsx
@@ -10,7 +10,7 @@ import Tabs from 'muicss/lib/react/tabs'
 import Tab from 'muicss/lib/react/tab'
 
 import { installTheme, syncToSafe } from './files.jsx'
-
+import { DropModal } from 'boron';
 
 class Configuration extends React.Component{
   constructor(props){
@@ -25,20 +25,28 @@ class Configuration extends React.Component{
       theme: '',
       permalink: '',
       dns_names: [],
+      newDns: '',
+      serializedUrl: [],
       themes: fs.readdirSync('/themes')
     }
   }
 
   componentDidMount(){
-    let config = require('../lib/statical-ghost/lib/config');
+    let config = require('../lib/statical-ghost/lib/config'),
+        url = config.blog.url,
+        serializedUrl = url ? url.split('.') : [];
     this.setState({
       title: config.blog.title,
       url: config.blog.url,
+      serializedUrl: serializedUrl,
       description: config.blog.description,
       posts_limit: config.posts.limit,
       permalink: config.posts.permalinks,
       theme: config.theme,
     })
+    window.Safe.dns.listNames().then((names) => {
+      this.setState({dns_names: names})
+    }).catch(console.error.bind(console))
   }
 
   save(){
@@ -55,14 +63,73 @@ class Configuration extends React.Component{
   installTheme(theme){
     let config = require('../lib/statical-ghost/lib/config');
     config.writeConfigToFile({theme: theme})
+    config.loadConfig()
     installTheme(theme)
     this.setState({theme: theme})
     syncToSafe(window.Safe.nfs, ['config.yaml'], '')
   }
 
+  registerDns(){
+    let newDns = this.state.newDns.trim();
+    if (!newDns) return
+    window.Safe.dns.createName(newDns).then(() => {
+      let names = this.state.dns_names;
+      names.push(newDns);
+      this.setState({dns_names: names, newDns: '', selectedDns: newDns});
+    }).catch(console.error.bind(console))
+  }
+
+  publish(){
+    let service = this.state.selectedService.trim(),
+        dns = this.state.selectedDns || (this.state.serializedUrl.length ? this.state.serializedUrl[1] : null);
+
+    if (!service || !dns){
+      alert("You need to select DNS and the service!")
+      return
+    }
+
+    window.Safe.dns.createServiceForName({
+      longName: dns,
+      serviceName: service,
+      serviceHomeDirPath: '/public',
+      isPathShared: false
+    }).then(() =>{
+      let url = service + '.' + dns + '.safenet';
+      let config = require('../lib/statical-ghost/lib/config');
+      config.writeConfigToFile({blog: {url: url}})
+      syncToSafe(window.Safe.nfs, ['config.yaml'], '')
+      this.setState({
+        url: url,
+        serializedUrl: url.split('.'),
+        selectedDns: null
+      });
+      this.refs.modal.hide()
+    }).catch(console.error.bind(console))
+  }
+
   render() {
     return (<div>
-      <Form>
+      <DropModal ref="modal">
+
+          <Input label="Service"
+                ref="service"
+                onChange={(e) => this.setState({"selectedService": e.target.value})}
+                defaultValue={this.state.serializedUrl.length ? this.state.serializedUrl[0] : '' } /> .
+          <Dropdown label={this.state.selectedDns ? this.state.selectedDns : (this.state.serializedUrl.length ? this.state.serializedUrl[1] : '') }>
+            {this.state.dns_names.map((t) =>
+              <DropdownItem onClick={()=> this.setState({selectedDns: t})}>
+                {t}</DropdownItem>
+            )}
+
+              <Input label="New DNS name" ref='newDns'
+              onChange={(e) => this.setState({"newDns": e.target.value})} /><Button onClick={() => this.registerDns()}>+</Button>
+
+          </Dropdown>
+          .safenet
+          <Button onClick={() => this.publish()} >publish</Button>
+
+      </DropModal>
+
         <Panel>
           <Button variant="raised" onClick={this.save.bind(this)} color="primary">Save </Button>
           <Input
@@ -99,8 +166,9 @@ class Configuration extends React.Component{
           )}
         </Dropdown>
 
+        <label>{this.state.url ? "Currently published under " + this.state.url : 'Currently not public'} <Button onClick={() => this.refs.modal.show()}>edit</Button></label>
       </Panel>
-      </Form>
+
     </div>)
   }
 }
